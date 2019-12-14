67433b3c7be3c7bfadbfbf0f27a77ad14467cc54
Matt Thomas <mthomas@guggenheim.org>
2019-12-14 18:51:46 -0500

    Seeding theme

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>blank</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+
+<body>
+</body>
+
+</html>
