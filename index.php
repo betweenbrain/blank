89f31700d602775baab5fb64cf653ac383535abd
Matt Thomas <mthomas@guggenheim.org>
2019-12-14 19:06:12 -0500

    Render the_loop and what it can display

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 <html>
 
 <head>
-    <title>blank</title>
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
+	<title>blank</title>
+	<meta name="viewport" content="initial-scale=1.0">
+	<meta charset="utf-8">
 </head>
 
 <body>
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		// https://developer.wordpress.org/themes/basics/the-loop/#what-the-loop-can-display
+        next_post_link();
+		previous_post_link();
+        the_author();
+		the_category();
+		the_content();
+		the_excerpt();
+		the_ID();
+		the_meta();
+        the_shortlink();
+		the_tags();
+		the_time();
+        the_title();
+	endwhile;
+endif;
+?>
 </body>
 
 </html>
