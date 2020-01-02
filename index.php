2a06f7d9aa83e29c03d7ac05b2277acbead65279
Matt Thomas <mthomas@guggenheim.org>
2020-01-01 19:56:51 -0500

    Adds basic rendering of locations

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
 	<title>blank</title>
 	<meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-    <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<?php wp_head(); ?>
 </head>
 
 <body>
@@ -14,21 +14,66 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		// https://developer.wordpress.org/themes/basics/the-loop/#what-the-loop-can-display
-        next_post_link();
+		next_post_link();
 		previous_post_link();
-        the_author();
+		the_author();
 		the_category();
 		the_content();
 		the_excerpt();
 		the_ID();
 		the_meta();
-        the_shortlink();
+		the_shortlink();
 		the_tags();
 		the_time();
-        the_title();
+		the_title();
 	endwhile;
 endif;
 ?>
+<div id="map"></div>
+<?php
+$key     = get_option( 'google_maps_api_key' );
+$markers = array();
+$terms   = get_terms(
+	array(
+		'taxonomy'   => 'location',
+		'hide_empty' => false,
+	)
+);
+foreach ( $terms as $term ) {
+	$location  = get_term_meta( $term->term_id, 'latLng', true );
+	$markers[] = esc_attr( $location );
+}
+?>
+	<style>
+		#map {
+			height: 800px;
+			margin: 0 auto;
+			width: 800px
+		}
+	</style>
+	<script>
+		const markers = <?php echo json_encode($markers) ?>;
+
+		function initMap() {
+			const map = new google.maps.Map(document.getElementById('map'), {
+				center: { lat: 41.763031, lng: -73.044465 },
+				zoom: 17
+			});
+
+			/* Render saved marker */ 
+			if(markers){
+				markers.forEach(elem => {
+					const cords = elem.replace('(', '').replace(')', '').split(',');
+					const position = new google.maps.LatLng(cords[0], cords[1]);
+					new google.maps.Marker({
+						position,
+						map
+					});
+				})
+			}
+		}
+	</script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=<?php echo get_option( 'google_maps_api_key' ); ?>&callback=initMap"async defer></script>
 </body>
 
 </html>
