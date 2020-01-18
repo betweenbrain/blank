31d5f49043d77fdb8e5066f7c6b405aba902bf48
Matt Thomas <mthomas@guggenheim.org>
2020-01-18 17:55:14 -0500

    Cleanup

--- index.php
+++ index.php
@@ -10,27 +10,6 @@
 
 <body>
 <?php
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		// https://developer.wordpress.org/themes/basics/the-loop/#what-the-loop-can-display
-		next_post_link();
-		previous_post_link();
-		the_author();
-		the_category();
-		the_content();
-		the_excerpt();
-		the_ID();
-		the_meta();
-		the_shortlink();
-		the_tags();
-		the_time();
-		the_title();
-	endwhile;
-endif;
-?>
-
-<?php
 /**
  * Render Google Map of today's program.
  */
